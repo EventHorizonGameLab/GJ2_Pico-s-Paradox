4966f987bb59994b0c08958b5832c11d65de5565
DrNox08 <marcopergamena.01@gmail.com>
2024-04-28 22:06:43 +0200

    Started Movement. Implemented InputManager w ActionMap

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputManager 
+{
+    public static ActionMap ActionMap;
+
+    static InputManager()
+    {
+        ActionMap = new ActionMap();
+        ActionMap.Enable();
+    }
+
+    public static Vector3 Movement => ActionMap.Player.Movement.ReadValue<Vector3>(); // Vettore già normalizzato
+
+    public static bool IsMoving (out Vector3 direction) // Funzione utilizzabile in update per muovere il personaggio, restituisce l'input direzionale
+    {
+        direction = Movement;
+        return direction != Vector3.zero;
+    }
+        
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent (typeof(BoxCollider))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Parameters")]
+    [SerializeField] float playerSpeed;
+    [Header("DO NOT TOUCH")]
+    [SerializeField] Transform targetMovePoint;
+    [SerializeField] LayerMask obstacle;
+    Vector3 movementVector;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void Start()
+    {
+        targetMovePoint.parent = null;
+    }
+
+    private void Update()
+    {
+        movementVector = InputManager.Movement;
+
+        transform.position = Vector3.MoveTowards(transform.position, targetMovePoint.position, playerSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, targetMovePoint.position) ==  0 )
+        {
+            if(DirectionIsAvailable(movementVector))
+            {
+                Vector3 newPosition = targetMovePoint.position + movementVector;
+
+                if (Physics.OverlapSphere(newPosition, 0.4f, obstacle).Length == 0)
+                {
+                    targetMovePoint.position = newPosition;
+                }
+            }
+        }
+
+    }
+
+    private bool DirectionIsAvailable(Vector3 newInputVector)
+    {
+        Vector3 xCheck = targetMovePoint.position + new Vector3(newInputVector.x, 0, 0);
+        Vector3 zCheck = targetMovePoint.position + new Vector3(0, 0, newInputVector.z);
+
+        bool xBlocked = Mathf.Abs(movementVector.x) > 0 && Physics.OverlapSphere(xCheck, 0.3f, obstacle).Length > 0;
+        bool zBlocked = Mathf.Abs(movementVector.z) > 0 && Physics.OverlapSphere(zCheck, 0.3f, obstacle).Length > 0;
+
+        return !xBlocked || !zBlocked;
+    }
+
+
+}
