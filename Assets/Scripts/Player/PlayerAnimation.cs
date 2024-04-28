c75b2bd9db5243ec9ee05ad90f110f6c2050b82d
DrNox08 <marcopergamena.01@gmail.com>
2024-04-28 23:59:46 +0200

    Player with test sprites and animations

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+    [SerializeField] Sprite[] idleSprites;
+    private Dictionary<Vector3, Sprite> directionToIdle;
+    Vector3 lastDirection;
+
+    enum SpriteIndex
+    {
+        FORWARD, BACKWARD, LEFT, RIGHT
+    }
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        InitializeDirectionToSpriteMap();
+    }
+
+    private void InitializeDirectionToSpriteMap()
+    {
+        directionToIdle = new Dictionary<Vector3, Sprite>
+    {
+        { Vector3.forward, idleSprites[(int)SpriteIndex.FORWARD] },
+        { Vector3.back, idleSprites[(int)SpriteIndex.BACKWARD] },
+        { Vector3.left, idleSprites[(int)SpriteIndex.LEFT] },
+        { Vector3.right, idleSprites[(int)SpriteIndex.RIGHT] }
+    };
+
+
+    }
+
+    void HandleSpriteAnimations(Vector3 direction)
+    {
+        if (direction != Vector3.zero)
+        {
+            anim.SetFloat("X", direction.x);
+            anim.SetFloat("Z", direction.z);
+
+        }
+        else
+        {
+            if (lastDirection != Vector3.zero)
+            {
+                spriteRenderer.sprite = directionToIdle[lastDirection];
+            }
+            anim.SetFloat("X", 0);
+            anim.SetFloat("Z", 0);
+        }
+    }
+
+    private void Update()
+    {
+        if(InputManager.IsMoving(out Vector3 direction))
+        {
+            lastDirection = direction;
+            HandleSpriteAnimations(direction);
+            anim.enabled = true;
+        }
+        else
+        {
+            HandleSpriteAnimations(Vector3.zero);
+            anim.enabled = false;
+        }
+    }
+
+}
